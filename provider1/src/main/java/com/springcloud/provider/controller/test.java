8eec8ffcf975b28dfd72c682315dfa30c1e42b9c
Downing <ntang7@dxc.com>
2019-12-30 15:47:39 +0800

    no message

--- provider1/src/main/java/com/springcloud/provider/controller/test.java
+++ provider1/src/main/java/com/springcloud/provider/controller/test.java
@@ -1,5 +1,6 @@
 package com.springcloud.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,14 @@ public class test {
     @RequestMapping("/hello")
     public String hello() {
         return "hello provider-02";
+    }
+
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/config")
+    public String config() {
+        return hello;
     }
 
     @GetMapping("/timeout")
--- provider2/src/main/java/com/springcloud/provider/controller/test.java
+++ provider2/src/main/java/com/springcloud/provider/controller/test.java
@@ -1,5 +1,7 @@
 package com.springcloud.provider.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +12,13 @@ public class test {
     @RequestMapping("/hello")
     public String hello() {
         return "hello provider-03";
+    }
+
+    @Value("${hello}")
+    private String hello;
+
+    @GetMapping("/config")
+    public String config() {
+        return hello;
     }
 }
